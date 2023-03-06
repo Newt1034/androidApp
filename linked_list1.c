d0ea626597b5ade5fa1cdaed61f3c201ff6e908e
abhijith <abhijithkumble87564@gmail.com>
2023-03-07 00:45:10 +0530

    some changes are made as told by you

--- linked_list1.c
+++ linked_list1.c
@@ -6,10 +6,18 @@ struct Node{
     struct Node *next;
 };
 
+void addNode(struct Node* head ,int data){
+	struct Node *newNode=(struct Node *)malloc(sizeof(struct Node));
+	newNode->data=data;
+	newNode->next=NULL;
+	head->next=newNode;
+}
+
+
 int main(){
     struct Node *head;
+    addNode(head,14);
     
-
     return 0;
 }
 
